7cb0ec2c20f447675cbc915834b32016e856df26
NicEric <niclasericsson89@hotmail.com>
2025-07-20 20:06:31 +0200

    adds functions for dir and file exploring

---
+++ tests.py
@@ -0,0 +1,54 @@
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
+
+
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+    result = get_file_content("calculator", "main.py")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+
+    result = get_file_content("calculator", "lorem.txt")
+    print(result)
+if __name__ == "__main__":
+    test()
+
+
+# from functions.get_file_content import get_file_content
+
+
+# def test():
+#     result = get_file_content("calculator", "main.py")
+#     print(result)
+
+#     result = get_file_content("calculator", "pkg/calculator.py")
+#     print(result)
+
+#     result = get_file_content("calculator", "/bin/cat")
+#     print(result)
+
+
+# if __name__ == "__main__":
+#     test()
